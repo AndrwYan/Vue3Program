92603b394769ff485137c08b7b1b5bafc2065927
SC-201902272017\Administrator <beyond199711@gmail.com>
2021-07-17 08:54:40 +0800

    ColumnList组件的编写

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,44 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="container">
+    <ColumnList :list="list"></ColumnList>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ColumnList,{ColumnProps} from './components/ColumnList.vue';
+
+const testData: ColumnProps[]=[
+  {
+    id: 1,
+    title: 'test1的专栏',
+    description: "关于生活！",
+    avatar: 'http://vue',
+  },
+  {
+    id: 2,
+    title: 'fsafa',
+    description: "nlscc",
+    avatar: 'http://',     
+  },  
+]
 
 export default defineComponent({
   name: 'App',
-  components: {
-    HelloWorld
+  components:{
+    ColumnList,
+  },
+  setup(){
+    return{
+      list: testData
+    }
   }
+
 });
 </script>
 
+//bootstrap中container的基础样式
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
